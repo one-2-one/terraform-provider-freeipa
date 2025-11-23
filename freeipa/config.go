4a678a769fbb5b36ae43ee26a23e757140541fd0
Oleksii Buriatov <oleksii.buriatov@together.com>
2025-11-23 10:26:51 +0200

    Add feature - keytab auth

--- freeipa/config.go
+++ freeipa/config.go
@@ -4,6 +4,7 @@ import (
 	"crypto/tls"
 	"log"
 	"net/http"
+	"os"
 
 	ipa "github.com/camptocamp/go-freeipa/freeipa"
 )
@@ -13,6 +14,11 @@ type Config struct {
 	Host               string
 	Username           string
 	Password           string
+	KerberosEnabled    bool
+	KerberosPrincipal  string
+	KerberosRealm      string
+	Krb5ConfPath       string
+	KeytabPath         string
 	InsecureSkipVerify bool
 }
 
@@ -24,7 +30,35 @@ func (c *Config) Client() (*ipa.Client, error) {
 		},
 	}
 
-	client, err := ipa.Connect(c.Host, tspt, c.Username, c.Password)
+	var (
+		client *ipa.Client
+		err    error
+	)
+
+	if c.KerberosEnabled {
+		krb5ConfFile, err := os.Open(c.Krb5ConfPath)
+		if err != nil {
+			return nil, err
+		}
+		defer krb5ConfFile.Close()
+
+		keytabFile, err := os.Open(c.KeytabPath)
+		if err != nil {
+			return nil, err
+		}
+		defer keytabFile.Close()
+
+		kerberosOpts := &ipa.KerberosConnectOptions{
+			Krb5ConfigReader: krb5ConfFile,
+			KeytabReader:     keytabFile,
+			Username:         c.KerberosPrincipal,
+			Realm:            c.KerberosRealm,
+		}
+
+		client, err = ipa.ConnectWithKerberos(c.Host, tspt, kerberosOpts)
+	} else {
+		client, err = ipa.Connect(c.Host, tspt, c.Username, c.Password)
+	}
 	if err != nil {
 		return nil, err
 	}
